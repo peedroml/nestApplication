6732c17dac71ce9e0e9e2fafa2068eb4ad5d93eb
Pedro <pedromoralesluque0@gmail.com>
2023-03-15 00:03:22 +0100

    Param, Body y REST Client

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,20 +1,33 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get , Post, Body, Param } from "@nestjs/common";
 
 //Podemos pasarle parámetros a los decoradores para controlar algunas
 //reglas de routing de alto nivel
-@Controller('/app')
+@Controller('mensajes')
 //Con 'export', exportaremos esta clase a otros archivos para asi utilizarla.
 export class AppController{
     
     //Cuando se haga una peticion Get a nuestra App, se ejecutará este metodo.
-    @Get('/hello')
-    sayHello(){
-        return '¡Hola buenas!';
+    @Get()
+    listarMensajes(){
+
     }
 
-    @Get('/bye')
-    sayBye(){
-        return 'Adiós'
+    //Cuando se reciba una peticion a través del decorador Post, extraeremos
+    //el cuerpo del mensaje con el decorador Body. 
+    @Post()
+    crearMensaje(@Body() body : any){
+        console.log(body)
     }
+
+    //Si queremos extraer un parametro la request entrante, usaremos el
+    //decorador Param. Le pasaremos un string para describir a que parte 
+    //de la url queremos acceder.
+    @Get('/:id')
+    getMensaje(@Param('id') id : string){
+        console.log(id)
+    }
+
+    
+    
 
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 
-//Creamos un modulo y le pasamos el controlador que acabamos de crear.
+//Creamos un modulo y le pasamos el controlador que hemos creado.
 @Module({
     controllers:[AppController]
 })
